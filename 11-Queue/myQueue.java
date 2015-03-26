288cde2710e2a432f9700396df938369a57acf77
Daisy B <daisybarbanel@yahoo.com>
2015-03-26 08:21:40 -0400

    finished queue

--- 11-Queue/myQueue.java
+++ 11-Queue/myQueue.java
@@ -13,27 +13,34 @@ public class myQueue<E>{
     public void enqueue(E data){
 	Node<E> tmp = new Node<E>(data);
 	if(first != null){
-	    first.setNext(tmp);
+	    last.setNext(tmp);
 	}else {
 	    first = tmp;
+	
 	}
-
+	
 	last = tmp;
 	length++;
     }
 
     public E dequeue(){
+	if(empty()){
+	    return null;
+	}
 	E data = first.getData();
 	first = first.getNext();
 	length--;
 	return data;
     }
 
-    public boolean empty(){
+    public boolean empty(){	
 	return first == null;
     }
 
-    public E front(){
+    public E head(){
+	if(empty()){
+	    return null;
+	}
 	return first.getData();
     }
 
@@ -41,17 +48,24 @@ public class myQueue<E>{
 	String s = "";
 	Node tmp = first;
 	for (int i = 0; i<length; i++){
-	    s += first + " ";
-	    first = first.getNext();
+	    s += tmp + " ";
+	    tmp = tmp.getNext();
 	}
 	return s;
     }
 
     public static void main(String[] args){
 	myQueue<Integer> q = new myQueue<Integer>();
-	System.out.println(q.empty());
-	q.enqueue(7);
 	System.out.println(q);
-	
+	q.dequeue();
+	q.enqueue(7);
+	q.enqueue(3);
+	q.enqueue(4);
+	System.out.println(q);
+	System.out.println(q.empty());
+	System.out.println(q.head());
+	q.dequeue();
+	System.out.println(q);
+	System.out.println(q.head());
     }
 }
