bc8a3cce80d8f98a3e1629c2c56b7d792fed31f6
Daisy B <daisybarbanel@yahoo.com>
2015-05-12 07:58:49 -0400

    new stuf

---
+++ 15-Trees/Node.java
@@ -0,0 +1,54 @@
+public class Node{
+
+    private int data;
+    private Node left,right;
+
+    public Node(){
+	left = null;
+	right = null;
+    }
+
+    public Node(int data){
+	this();
+	this.data=data;
+    }
+
+    public Node getLeft(){
+	return left;
+    }
+
+    public Node getRight(){
+	return right;
+    }
+
+    public int getData(){
+	return data;
+    }
+
+    public void setLeft(Node n){
+	left = n;
+    }
+
+    public void setRight(Node n){
+	right = n;
+    }
+
+    public void setData(int n){
+	data = n;
+    }
+
+    public int numKids(){
+	int n = 0;
+	if(left!=null){
+	    n++;
+	}
+	if(right!=null){
+	    n++;
+	}
+	return n;
+    }
+
+    public String toString(){
+	return data + ",";
+    }
+}
